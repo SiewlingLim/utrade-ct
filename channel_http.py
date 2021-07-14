2e9c16b4525b2a53a6e717769f57657e19987ddd
SiewlingLim <sllim0803@yahoo.com>
2021-07-14 14:02:18 +0800

    testing

--- channel_http.py
+++ channel_http.py
@@ -58,8 +58,9 @@ class CanarytokenPage(resource.Resource, InputChannel):
             #location and refere are for cloned sites
             location  = request.args.get('l', [None])[0]
             referer   = request.args.get('r', [None])[0]
-            xx = {'Request Data': {k:v for k,v in request.args.iteritems() }}
-            log.info(str(xx))
+            for k,v in request.args.iteritems():
+                log.info(k)
+                log.info(v)
             #log.info(useragent)
             #log.info(location)
             #log.info(referer)
